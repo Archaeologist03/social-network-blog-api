594f101ce97094d7685400f8543f37b03d29acc4
Archaeologist03 <omniiknight@hotmail.com>
2019-04-12 10:43:45 +0200

    Static images, Error handling(middleware). 374.

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
@@ -8,6 +10,7 @@ const feedRoutes = require('./routes/feed');
 
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 // CORS "fix"(bypass)
 app.use((req, res, next) => {
@@ -24,6 +27,13 @@ app.use((req, res, next) => {
 });
 
 app.use('/feed', feedRoutes);
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  res.status(status).json({ message: message });
+});
 
 mongoose
   .connect(
