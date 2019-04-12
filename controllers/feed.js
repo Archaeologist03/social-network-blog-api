900a2f065d0af9833d1d7fac9797ae53e7a073ff
Archaeologist03 <omniiknight@hotmail.com>
2019-04-12 11:23:30 +0200

    Fetching sigle post(with params postId). Uploading images with multer. 377.

--- controllers/feed.js
+++ controllers/feed.js
@@ -3,20 +3,18 @@ const { validationResult } = require('express-validator/check');
 const Post = require('../models/post');
 
 exports.getPosts = (req, res, next) => {
-  res.status(200).json({
-    posts: [
-      {
-        _id: 1,
-        title: 'First post',
-        content: 'This is the first post!',
-        imageUrl: 'images/screenshot.png',
-        creator: {
-          name: 'Ilija',
-        },
-        createdAt: new Date(),
-      },
-    ],
-  });
+  Post.find()
+    .then(posts => {
+      res
+        .status(200)
+        .json({ message: 'Fetched posts successfully', posts: posts });
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.status = 500;
+      }
+      next(err);
+    });
 };
 
 exports.createPost = (req, res, next) => {
@@ -27,14 +25,21 @@ exports.createPost = (req, res, next) => {
     throw error;
   }
 
+  if (!req.file) {
+    const error = new Error('No image provided');
+    error.statusCode = 422;
+    throw error;
+  }
+
+  // req.file.path = multer generates path that holds the path on to the file on server
+  const imageUrl = req.file.path;
   const title = req.body.title;
   const content = req.body.content;
-
   // Create post in db
   const post = new Post({
     title: title,
     content: content,
-    imageUrl: 'images/screenshot.png',
+    imageUrl: imageUrl,
     creator: { name: 'Ilija' },
   });
   post
@@ -48,6 +53,25 @@ exports.createPost = (req, res, next) => {
     })
     .catch(err => {
       if (!err.statusCode) {
+        err.status = 500;
+      }
+      next(err);
+    });
+};
+
+exports.getPost = (req, res, next) => {
+  const postId = req.params.postId;
+  Post.findById(postId)
+    .then(post => {
+      if (!post) {
+        const error = new Error('Could not find post');
+        error.statusCode = 404;
+        throw error;
+      }
+      res.status(200).json({ message: 'Post fetched.', post: post });
+    })
+    .catch(err => {
+      if (err.statusCode) {
         err.status = 500;
       }
       next(err);
--- routes/feed.js
+++ routes/feed.js
@@ -14,12 +14,14 @@ router.post(
   [
     body('title')
       .trim()
-      .isLength({ min: 7 }),
+      .isLength({ min: 5 }),
     body('content')
       .trim()
       .isLength({ min: 5 }),
   ],
   feedController.createPost,
 );
+
+router.get('/post/:postId', feedController.getPost);
 
 module.exports = router;
